2710a460499a2687c4231f39004a07a893c824d5
veerendra-dazn <140157377+veerendra-dazn@users.noreply.github.com>
2024-05-21 20:48:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import Router from "./Router";
-import { BrowserRouter, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const App = () => {
+  const isLogged = false;
+
   return (
     <div>
-      <BrowserRouter>
-        <header>
-          <nav>
-            <NavLink to="/resume">Resume</NavLink>
-            <NavLink to="/simple-button">Simple Button</NavLink>
-            <NavLink to="/counter">Counter</NavLink>
-            <NavLink to="/users-table">Users Table</NavLink>
-            <NavLink to="/users-api">Users API </NavLink>
-            <NavLink to="/button">Button</NavLink>
-            <NavLink to="/todo-app">Todo App</NavLink>
-            <NavLink to="/register">Register</NavLink>
-            <NavLink to="/login">Login</NavLink>
-          </nav>
-        </header>
-        <Router />
-      </BrowserRouter>
+      <header>
+        <nav>
+          <NavLink to="/resume">Resume</NavLink>
+          <NavLink to="/simple-button">Simple Button</NavLink>
+          <NavLink to="/counter">Counter</NavLink>
+          <NavLink to="/users-table">Users Table</NavLink>
+          <NavLink to="/users-api">Users API </NavLink>
+          <NavLink to="/button">Button</NavLink>
+          <NavLink to="/todo-app">Todo App</NavLink>
+          <NavLink to="/register">Register</NavLink>
+          <NavLink to="/login">Login</NavLink>
+        </nav>
+      </header>
+      <Router isLogged={isLogged} />
     </div>
   );
 };
